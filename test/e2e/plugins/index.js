1aca97e325a9fec97fcdf86415f2ef4a3eb985dc
Loïc Fuentes <loic.fuentes@aerial.ai>
2021-06-19 07:47:21 -0400

    f/clean-base-model

--- test/e2e/plugins/index.js
+++ test/e2e/plugins/index.js
@@ -16,10 +16,10 @@ module.exports = (on, config) => {
   // }))
 
   return Object.assign({}, config, {
-    fixturesFolder: "test/e2e/fixtures",
-    integrationFolder: "test/e2e/specs",
-    screenshotsFolder: "test/e2e/screenshots",
-    videosFolder: "test/e2e/videos",
-    supportFile: "test/e2e/support/index.js",
-  });
-};
+    fixturesFolder: 'test/e2e/fixtures',
+    integrationFolder: 'test/e2e/specs',
+    screenshotsFolder: 'test/e2e/screenshots',
+    videosFolder: 'test/e2e/videos',
+    supportFile: 'test/e2e/support/index.js'
+  })
+}
